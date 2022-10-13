068b7074291c3eb57b271d86d75e8afc067b5b51
Максим <Mt.Pm@yandex.ru>
2022-10-13 20:16:59 +0700

    ссделал задание Hello_Koko!

---
+++ Hello_Koko!/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Назовите своё имя: ");
+string username=Console.ReadLine();
+
+if(username.ToLower() == "ксюша")
+{
+    Console.WriteLine("Привет, любимая Коко! ");
+}
+else
+{
+    Console.Write("А,это ты,чмошник, ну здорова, ");
+    Console.WriteLine(username);
+}
+
