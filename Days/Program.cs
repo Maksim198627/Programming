69b9a3a90a303ec9ad3cefbad680bb4aca0aef00
Максим <Mt.Pm@yandex.ru>
2022-10-16 09:49:54 +0700

    Куча новых папок.Решена задача  найти квадрат

---
+++ Days/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите номер дня недели: ");
+int input1 = 3;
+int input2 = 5;
+string day1 = wednesday;
+string day2 = friday;
+int res = input1 = day1; input2 = day2;
+Console.WriteLine(": ");
---
+++ Trenning/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите первое число: ");
+int inputA= int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число: ");
+int inputB = int.Parse(Console.ReadLine()!);
+int result =  inputA = inputB * inputB;
+Console.WriteLine(result);
+
+
