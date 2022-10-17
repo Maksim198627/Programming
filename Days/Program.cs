00c4256cd180d3b962a1754d9a44665c78f237ce
Максим <Mt.Pm@yandex.ru>
2022-10-17 11:57:52 +0700

    Решены папки days и Trenning

--- Days/Program.cs
+++ Days/Program.cs
@@ -1,7 +1,11 @@
 ﻿Console.WriteLine("Введите номер дня недели: ");
-int input1 = 3;
-int input2 = 5;
-string day1 = wednesday;
-string day2 = friday;
-int res = input1 = day1; input2 = day2;
-Console.WriteLine(": ");
+int number1 = int.Parse(Console.ReadLine()!);
+
+if (number1 == 3)
+{
+    Console.WriteLine("Wednesday");
+}
+if (number1 == 5)
+{
+    Console.WriteLine("Friday");
+}
--- Trenning/Program.cs
+++ Trenning/Program.cs
@@ -2,7 +2,14 @@
 int inputA= int.Parse(Console.ReadLine()!);
 Console.WriteLine("Введите второе число: ");
 int inputB = int.Parse(Console.ReadLine()!);
-int result =  inputA = inputB * inputB;
-Console.WriteLine(result);
+
+if(inputA == inputB * inputB)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
 
 
