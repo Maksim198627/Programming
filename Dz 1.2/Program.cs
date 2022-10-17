f4e1396bdfb40719a628a43e8e68d061a7bfdd6a
Максим <Mt.Pm@yandex.ru>
2022-10-17 12:53:34 +0700

    DZ 1.2

---
+++ Dz 1.2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите первое число");
+int number1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число");
+int number2 = int.Parse(Console.ReadLine()!);
+
+if(number1 > number2)
+{
+  Console.WriteLine("Большее число: " + number1);  
+}
+if(number1 < number2)
+{
+  Console.WriteLine("Большее число: " + number2);  
+}
+
